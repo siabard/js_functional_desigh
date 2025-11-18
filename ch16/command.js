8b7c51793067de4addc4c1587b3b69f29231a682
Yeonho Jang <siabard@gmail.com>
2025-11-18 19:18:53 +0900

    command 패턴 코드 추가

--- ch16/command.js
+++ ch16/command.js
@@ -24,28 +24,52 @@ const getType = (command) => {
 const execute = createMultiMethod(getType);
 const undo = createMultiMethod(getType);
 
+let canvas = [];
+
 const addRoom = () => {
-  return 'room added';
+   const roomNumber = Math.floor(Math.random() * 100);
+   const newRoom = {'roomNumber': roomNumber};
+    
+   canvas.push(newRoom);
+   return roomNumber;
 }
 
 const deleteRoom = (room) => {
   // working delete room
-  console.log('room deleted');
+  canvas = canvas.filter(cv => cv.roomNumber != room.roomNumber);
 }
 
-const makeAddRommCommand = (command) => {
-  return {"ctype" : "add-room-command"};
+const makeAddRoomCommand = () => {
+    return {"ctype" : "add-room-command"};
 };
 
 execute.addMethod("add-room-command", (command) => {
-  return {
-    ...makeAddRommCommand(),
-    "the-added-room": command
-  };
+  return {...command, 'roomNumber' : addRoom()};
+  
 })
 
 undo.addMethod("add-room-command", (command) => {
-  return {
-    "the-added-room": command
-  };
+  deleteRoom(command);
 });
+
+let undo_list = [];
+
+// 방을 연속으로 만든다.
+
+undo_list.push(execute(makeAddRoomCommand()));
+undo_list.push(execute(makeAddRoomCommand()));
+undo_list.push(execute(makeAddRoomCommand()));
+
+console.log("Canvas => ", canvas);
+
+// 등록된 작업 목록
+
+console.log("Undo List => ", undo_list);
+
+// 가장 최근 작업을 취소한다.
+
+undo( undo_list.pop());
+
+// Canvas 현황 
+
+console.log("Canvas => ", canvas);
